6c0a8293bc870e39cb170ba8a717e31fdd457194
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-06-23 01:17:02 +0530

    enjoy your tv

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -65,20 +65,48 @@ const Login = () => {
   };
 
   return (
-    <div className="">
-      <Header />
-      <div className="absolute">
-        <img src={LOGIN_BG} className="h-screen md:h-full bg-cover w-screen" alt="Netflix BG" />
-      </div>
-      <form onSubmit={(e) => e.preventDefault()} className="absolute p-12 sm:w-1/2 md:w-3/12 h-[500px] mt-36 mx-auto text-white bg-black bg-opacity-80 right-0 rounded-lg left-0 flex flex-col justify-center items-center ">
-        <h1 className="font-bold text-3xl py-4 mr-auto">{ isSignInForm ? <>{lang[langKey].signIn}</> : <>{lang[langKey].signUp}</> }</h1>
-        {!isSignInForm && <input type="text" ref={name} placeholder={lang[langKey].fnmPlaceholder} className="px-3 py-2 m-2 w-full bg-gray-600 " required />}
-        <input type="text" ref={email} placeholder={lang[langKey].emailPlaceholder} className="px-3 py-2 my-3 w-full bg-gray-600 " required />
-        <input type="password" ref={password} placeholder={lang[langKey].passwordPlaceholder} className="px-3 py-2 my-3 w-full bg-gray-600 " required />
-        <p className="text-red-500 font-bold text-lg p-2">{errorMessage}</p>
-        <button className="p-3 my-6 bg-red-600 rounded-lg w-full" onClick={handleButtonClick}>{isSignInForm ? <>{lang[langKey].signIn}</> : <>{lang[langKey].signUp}</> }</button>
-        <p className="p-4 mr-auto text-lg text-gray-400" >{isSignInForm ? <>{lang[langKey].newToNetflix}</> : <>{lang[langKey].alreadyRegister}</> } <span onClick={toggleSignUpForm} className="text-white cursor-pointer hover:underline font-bold"> { isSignInForm ? <>{lang[langKey].signUpNow}</> : <>{lang[langKey].signInNow}</> }</span></p>
-      </form>
+    <div className="flex flex-col">
+        <Header />
+        <div className="">
+          <img src={LOGIN_BG} className="h-screen md:h-full bg-cover w-screen" alt="Netflix BG" />
+          <hr className="border-[#232323] border-4" />
+          <div className=" w-screen h-[550px] bg-black flex justify-center items-center">
+            <div className="flex flex-col gap-5">
+              <h1 className="text-white text-5xl font-black">Enjoy on your TV</h1>
+              <p className="w-[550px] text-white text-2xl font-normal">Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</p>
+            </div>
+            <div className="flex flex-col justify-center items-center overflow-hidden">
+              <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" className="h-[450px]" />
+              <div className="absolute w-[406px] mb-[15px] mr-[5px] ">
+                <video className="inline-block" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v" autoPlay loop />
+              </div>
+            </div>
+          </div>
+          <hr className="border-[#232323] border-4" />
+          <div className=" w-screen h-[550px] bg-black flex justify-center items-center">
+            <div className="flex flex-col justify-center items-center overflow-hidden">
+              <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" className="h-[450px]" />
+              <div className="absolute border-2 border-[#232323] w-[400px] h-36">
+                
+              </div>
+            </div>
+            <div className="flex flex-col gap-5">
+              <h1 className="text-white text-5xl font-black w-[480px]">Download your shows to watch offline</h1>
+              <p className="w-[560px] text-white text-2xl font-normal">Save your favourites easily and always have something to watch.</p>
+            </div>
+          </div>
+        </div>
+        <hr className="border-[#232323] border-4" />
+        <form onSubmit={(e) => e.preventDefault()} className="absolute p-12 sm:w-1/2 md:w-3/12 h-[500px] mt-36 mx-auto text-white bg-black bg-opacity-80 right-0 rounded-lg left-0 flex flex-col justify-center items-center ">
+          <h1 className="font-bold text-3xl py-4 mr-auto">{ isSignInForm ? <>{lang[langKey].signIn}</> : <>{lang[langKey].signUp}</> }</h1>
+          {!isSignInForm && <input type="text" ref={name} placeholder={lang[langKey].fnmPlaceholder} className="px-3 py-2 m-2 w-full bg-gray-600 " required />}
+          <input type="text" ref={email} placeholder={lang[langKey].emailPlaceholder} className="px-3 py-2 my-3 w-full bg-gray-600 " required />
+          <input type="password" ref={password} placeholder={lang[langKey].passwordPlaceholder} className="px-3 py-2 my-3 w-full bg-gray-600 " required />
+          <p className="text-red-500 font-bold text-lg p-2">{errorMessage}</p>
+          <button className="p-3 my-6 bg-red-600 rounded-lg w-full" onClick={handleButtonClick}>{isSignInForm ? <>{lang[langKey].signIn}</> : <>{lang[langKey].signUp}</> }</button>
+          <p className="p-4 mr-auto text-lg text-gray-400" >{isSignInForm ? <>{lang[langKey].newToNetflix}</> : <>{lang[langKey].alreadyRegister}</> } <span onClick={toggleSignUpForm} className="text-white cursor-pointer hover:underline font-bold"> { isSignInForm ? <>{lang[langKey].signUpNow}</> : <>{lang[langKey].signInNow}</> }</span></p>
+        </form>
+      
     </div>
   )
 }
