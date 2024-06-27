eecd5b5122d3835ce02f6e488af2d4344df28716
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-06-27 16:56:01 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,31 +3,31 @@ const Footer = () => {
   return (
     <div className="bg-black h-[400px] justify-center w-screen flex">
         <div className="w-8/12 flex items-center">
+        <div className="flex flex-col w-72 gap-3">
+            <a className="text-white text-base font-thin">Questions? call <span className="underline"> 000-800-919-1694 </span> </a>
+            <a className="text-white cursor-pointer text-base font-thin underline">FAQ</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Investor Relations</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Privacy</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Speed Test</a>
+            <a className="text-white text-lg font-normal">Netflix India</a>
+        </div>
+        <div className="flex flex-col w-72 gap-3">
+            <a className="text-white cursor-pointer text-base font-thin underline">Help Center</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Jobs</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Cookies Prefrence</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Legal Noties</a>
+        </div>
         <div className="flex flex-col w-64 gap-3">
-            <a className="text-white cursor-pointer text-lg font-normal underline">Questions? Call 000-800-919-1694</a>
-            <a className="text-white cursor-pointer text-lg font-normal">FAQ</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Investor Relations</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Privacy</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Speed Test</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Netflix India</a>
-        </div>
-        <div className="flex flex-col w-56 gap-3">
-            <a className="text-white cursor-pointer text-lg font-normal">Help Center</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Jobs</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Cookies Prefrence</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Legal Noties</a>
-        </div>
-        <div className="flex flex-col w-60 gap-3">
-            <a className="text-white cursor-pointer text-lg font-normal">Account</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Ways to Watch</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Corporate Information</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Only on Netflix</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Account</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Ways to Watch</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Corporate Information</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Only on Netflix</a>
         </div>
         <div className="flex flex-col gap-3 ">
-            <a className="text-white cursor-pointer text-lg font-normal">Media Center</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Terma of Use</a>
-            <a className="text-white cursor-pointer text-lg font-normal">Contact Us</a>
-        </div>
+            <a className="text-white cursor-pointer text-base font-thin underline">Media Center</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Terma of Use</a>
+            <a className="text-white cursor-pointer text-base font-thin underline">Contact Us</a>
+     underline    </div>
         </div>
     </div>
   )
